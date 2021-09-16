12971f025272e7fb989281648e10155472b77655
judongseok <wnehdtjr5@gmail.com>
2021-09-17 00:25:19 +0900

    first initial

---
+++ GiftY/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GiftY
+//
+//  Created by judongseok on 2021/09/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
