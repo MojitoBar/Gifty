ad2ef086fdf45cb79feaf5bb7bd820d923e0a0c8
MojitoBar <wnehdtjr5@naver.com>
2022-04-26 20:29:33 +0900

    change: directory struct

--- QRCode-Example/View/ViewController.swift
+++ QRCode-Example/View/ViewController.swift
@@ -5,7 +5,6 @@
 //  Created by Hans Knöchel on 09.06.17.
 //  Copyright © 2017 Hans Knoechel. All rights reserved.
 //
-import AVKit
 import UIKit
 import Vision
 import Photos
@@ -104,6 +103,21 @@ class ViewController: UIViewController {
     public var loading = 0
     public var isLoading = false
     
+    // MARK: - Loading Animation Indicator
+    lazy var activityIndicator: UIActivityIndicatorView = {
+        // Create an indicator.
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        activityIndicator.center = self.view.center
+        activityIndicator.color = UIColor.red
+        // Also show the indicator even when the animation is stopped.
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorView.Style.white
+        // Start animation.
+        activityIndicator.stopAnimating()
+        return activityIndicator
+    }()
+    
     // MARK: - 이미지 스캔
     func scanImage(data: Data, photo: PHAsset) {
         let barcodeRequest = VNDetectBarcodesRequest(completionHandler: { [self] request, error in
@@ -175,20 +189,6 @@ class ViewController: UIViewController {
         }
     }
     
-    // MARK: - Loading Animation Indicator
-    lazy var activityIndicator: UIActivityIndicatorView = {
-        // Create an indicator.
-        let activityIndicator = UIActivityIndicatorView()
-        activityIndicator.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
-        activityIndicator.center = self.view.center
-        activityIndicator.color = UIColor.red
-        // Also show the indicator even when the animation is stopped.
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorView.Style.white
-        // Start animation.
-        activityIndicator.stopAnimating()
-        return activityIndicator }()
-    
     // MARK: - PHAsset to UIImage Function
     func getAssetThumbnail(asset: PHAsset) -> UIImage {
         let manager = PHImageManager.default()
@@ -204,68 +204,12 @@ class ViewController: UIViewController {
         return thumbnail
     }
 }
-
-// MARK: - Extension UICollectionViewDelegate & UICollectionViewDataSource
-extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource{
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        DispatchQueue.main.async { [self] in
-            loadImageLabel.text = "불러온 이미지는 \(barcodeDatas.count) 개 입니다."
-        }
-        
-        return barcodeDatas.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CSCollectionViewCell
-        
-        cell.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
-        cell.image.image = barcodeDatas[indexPath.row]
-        cell.image.contentMode = .scaleAspectFill
-        cell.image.frame = CGRect(x: 0, y: 0, width: collectionView.frame.width / 3 - 10, height: collectionView.frame.width / 3 - 10)
-        
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let vcName = self.storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as? DetailViewController
-        vcName?.modalTransitionStyle = .coverVertical
-        vcName?.modalPresentationStyle = .fullScreen
-        vcName?.paramImage = barcodeDatas[indexPath.row]
-        vcName?.paramAsset = results[indexPath.row]
-        vcName?.paramFetchResult = fetchPhotos
-        index = indexPath.row
-        self.present(vcName!, animated: true, completion: nil)
-    }
-}
-
-// MARK: - Extension UICollectionViewDelegateFlowLayout
-extension ViewController: UICollectionViewDelegateFlowLayout {
-    
-    // 위 아래 간격
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
-    }
-    
-    // 옆 간격
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
-    }
-    
-    // cell 사이즈( 옆 라인을 고려하여 설정 )
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        let width = collectionView.frame.width / 3 - 10 ///  3등분하여 배치, 옆 간격이 1이므로 1을 빼줌
-        let size = CGSize(width: width, height: width)
-        return size
-    }
-}
-
 // MARK: - ViewController Life Cycle
 extension ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.addSubview(self.activityIndicator)
-        setUI()
+        reloadBtn.setTitle("", for: .normal)
         
         loadingPer.text = "0"
         
@@ -303,12 +247,5 @@ extension ViewController {
         
         fetchContact()
         collectionView.reloadData()
-    }
-}
-
-extension ViewController {
-    // UI SETUP
-    func setUI() {
-        reloadBtn.setTitle("", for: .normal)
     }
 }
